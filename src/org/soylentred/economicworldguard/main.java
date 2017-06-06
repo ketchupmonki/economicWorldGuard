fa3eea5f8e55806acc266b8107d722c7f9a175bd
ketchupmonki <alextheulings@gmail.com>
2017-06-06 19:14:03 +0100

    Changed evict command to a plugin-wide admin command.

--- src/org/soylentred/economicworldguard/main.java
+++ src/org/soylentred/economicworldguard/main.java
@@ -36,7 +36,7 @@ public class main extends JavaPlugin{
 
 		//Register Commands
 		this.getCommand("buychunk").setExecutor(new commandBuyChunk(config.getBoolean("debug"), bank, config.getInt("chunkBuyPrice"), getWorldGuard(), config.getStringList("ignoreRegions")));
-		this.getCommand("evictchunk").setExecutor(new commandEvictChunk(config.getBoolean("debug"), getWorldGuard()));
+		this.getCommand("ewgadmin").setExecutor(new commandEwgadmin(config.getBoolean("debug"), getWorldGuard()));
 		this.getCommand("givechunk").setExecutor(new commandGiveChunk(config.getBoolean("debug"), getWorldGuard()));
 		this.getCommand("sellchunk").setExecutor(new commandSellChunk(config.getBoolean("debug"), bank, config.getInt("chunkSellPrice"), getWorldGuard()));
 		this.getCommand("sharechunk").setExecutor(new commandShareChunk(config.getBoolean("debug"), getWorldGuard()));
